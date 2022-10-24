d5b0f54e4e717dd6cdd545d70a7a8b2a16d7cc83
PRITAM KUMAR MANI <pritammanijoy2001@gmail.com>
2022-10-24 23:36:01 +0530

    new code

---
+++ ArrayForEach.js
@@ -0,0 +1,12 @@
+let num = [3,5,7,3,12,4,6]
+
+for (let i = 0; i < num.length; i++) {
+    console.log(num[i])
+    
+}
+
+console.log("-----------------------------------")
+//ForEach Loop 
+num.forEach((element) => {
+    console.log(element * element)
+})
