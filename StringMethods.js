6c1309991cfc62abfa82a8c8d921f13cb77e7982
PRITAM KUMAR MANI <pritammanijoy2001@gmail.com>
2022-10-24 23:00:24 +0530

    5

---
+++ StringMethods.js
@@ -0,0 +1,18 @@
+let name = "PRITAM"
+
+console.log(name.length);
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+
+console.log(name.slice(2 ,4 ))
+
+console.log(name.replace("PRITAM", "MANI"))
+
+let friend = "MAnab"
+console.log(name.concat(" is a friend of ", friend, "ok"))
+
+
+ let friend2= "       Meena   "
+ console.log(friend2.trim())
+ console.log(friend2)
+
