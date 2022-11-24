76ee7203024f57ed7bcfd540fd041422cb9e7029
PRITAM KUMAR MANI <pritammanijoy2001@gmail.com>
2022-11-24 23:11:48 +0530

    Update

---
+++ login-system/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = process.env.PORT||3110;
+
+app.set('view engine','ejs');
+
+//load static assets
+app.use('/static',express.static(pah.join(__dirname,'public')))
+//home route
+app.get('/',(req,res)=>{
+    res.render('base',{title:"Login System"});
+})
+
+app.listen(port,() => {
+console.log("Listening to the server on http://localhost:3110");
+});
