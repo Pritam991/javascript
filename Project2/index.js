c82b5879ebcadd2cd097e20fc5e764cce411596b
PRITAM KUMAR MANI <pritammanijoy2001@gmail.com>
2022-11-29 09:06:16 +0530

    Update

--- Project2/index.js
+++ Project2/index.js
@@ -1,6 +1,8 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const port = 3000;
+const fetch = require('node-fetch');
 
 app.use(express.static('public'));
 
@@ -8,3 +10,20 @@ app.listen(port, () =>{
     console.log(`Example app listening at http://localhost:${port}`);
 });
 
+app.get('/dinoname', async(req,res)=>{
+    //run code stuff
+    const fetchApi =  await fetch(
+        'https://dinoipsum.com/api/?format=json&words=10&paragraphs=3',
+        {
+            method: 'GET',
+            headers: {
+                'x-rapdapi-key' : 'd941c8fdebmshf34b43c3e6fd551p14e7d1jsn39192b780730',
+                'x-rapidal-host' : 'alexnormand-dino-ipsum.p.rapidapi.com',
+            },
+        }
+    );
+    const dinoNameResponse = await fetchApi.json();
+    console.log(dinoNameResponse);
+    response.json(dinoNameResponse);
+
+});
--- Project2/public/script.js
+++ Project2/public/script.js
@@ -1,1 +1,9 @@
 console.log('script.js loaded');
+
+getDinoName();
+
+  async function getDinoName(){
+    const response = await fetch('/dinoname');
+    const data = await response.json();
+    console.log(data[0]);
+}
