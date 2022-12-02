43ba6936a6ad39e58ffb286a24ff4567d801996a
PRITAM KUMAR MANI <pritammanijoy2001@gmail.com>
2022-12-02 18:00:52 +0530

    update

--- Project4-CreateRESTAPI/index.js
+++ Project4-CreateRESTAPI/index.js
@@ -1,7 +1,7 @@
-import  express  from "expresss";
-import bodyParser from "body-parser";
+import express from 'express';
+import bodyParser from 'body-parser';
 
-import userRoutes from ' ./routes/users.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
 const PORT = 3003;
--- Project4-CreateRESTAPI/routes/users.js
+++ Project4-CreateRESTAPI/routes/users.js
@@ -1,9 +1,39 @@
-import { Express } from "express";
+import  express  from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
 
 const router = express.Router();
 
-router.get('/users', (req, res) =>{
-    res.send('Hello');
+const users = [];
+
+
+//all routes in here are starting with /users
+router.get('/', (req, res) =>{
+   
+    res.send(users);
 } );
+
+router.post('/', (req, res) =>{
+    //console.log('POST ROUTE REACHED');
+    const user = req.body;
+
+ //  const userId= uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+  
+  // const userWithId = { ...user, id: uuidv4() }
+
+   users.push({ ...user, id: uuidv4() });
+
+   // users.push(user);
+
+    res.send(`User with the name ${user.fname} added to the database!`);
+
+    //console.log(req.body);
+
+    //users.push();
+  //  res.send('POST ROUTE RERACHED');
+    
+});
+
+
 
 export default router;
