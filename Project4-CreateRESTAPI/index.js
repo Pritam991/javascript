a6c3c150fb35856b5c8b9b68933f8824229bcc2d
PRITAM KUMAR MANI <pritammanijoy2001@gmail.com>
2022-12-02 10:21:37 +0530

    update

--- Project4-CreateRESTAPI/index.js
+++ Project4-CreateRESTAPI/index.js
@@ -6,6 +6,12 @@ const PORT = 3003;
 
 app.use(bodyParser.json());
 
+app.get('/', (req, res) =>{
+ 
+
+ res.send('Hello from Homepage');
+});
+
 app.listen(PORT, () =>
 console.log(`Server running on port: http://localhost:${PORT}`))
 
