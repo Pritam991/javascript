d22b2662c0b320d56409a06b924f71bd3f00bff6
PRITAM KUMAR MANI <pritammanijoy2001@gmail.com>
2022-12-03 16:37:46 +0530

    update

--- Project4-CreateRESTAPI/routes/users.js
+++ Project4-CreateRESTAPI/routes/users.js
@@ -34,6 +34,22 @@ router.post('/', (req, res) =>{
     
 });
 
+// /users/2 => req.paras {id:2}
+
+router.get('/:id', (req, res) =>{
+    const {id} = req.params;
+
+    const foundUser = users.find((user) => user.id === id);
+
+    res.send(foundUser);
+});
+
+router.delete('/:id', (req, res) =>{
+    const {id} = req.params;
+
+    users = users.filter((user) => user.id !== id);
+})
+
 
 
 export default router;
