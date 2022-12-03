9307305ce54d96b2104ec41d6d957d9dd6956008
PRITAM KUMAR MANI <pritammanijoy2001@gmail.com>
2022-12-03 17:10:17 +0530

    update

--- Project4-CreateRESTAPI/routes/users.js
+++ Project4-CreateRESTAPI/routes/users.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [];
+let users = [];
 
 
 //all routes in here are starting with /users
@@ -48,8 +48,29 @@ router.delete('/:id', (req, res) =>{
     const {id} = req.params;
 
     users = users.filter((user) => user.id !== id);
-})
 
+    res.send(`User with the id ${id} deleted from the database.`);
+});
+
+router.patch('/:id', (req, res) => {
+    const { id } = req.params;
+    const {fname, lname, age } = req.body;
+    const user = users.find((user) => user.id === id);
+
+    if(fname){
+        user.fname = fname;
+    }
+
+    if(lname){
+        user.lname = lname;
+    }
+    if(age){
+        user.age = age;
+    }
+
+    res.send(`User with the id ${id} has been updated.`);
+
+});
 
 
 export default router;
