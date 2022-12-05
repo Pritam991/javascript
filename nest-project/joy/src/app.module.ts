a326f93e6b616052d1807185ccd77d39ec2325ae
PRITAM KUMAR MANI <pritammanijoy2001@gmail.com>
2022-12-05 16:02:10 +0530

    Update

--- nest-project/joy/src/app.module.ts
+++ nest-project/joy/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CarModule } from './car/car.module';
+import { CarService } from './car/car.service';
 
 @Module({
   imports: [CarModule],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [CarService],
 })
 export class AppModule {}
