83a3d420708e20a13d3a964d4c1e9a58bf7b40d3
PRITAM KUMAR MANI <pritammanijoy2001@gmail.com>
2022-12-06 11:41:26 +0530

    update

--- nest-project/joy/src/car/car.controller.ts
+++ nest-project/joy/src/car/car.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Get } from '@nestjs/common';
 import {CarService} from './car.service';
 @Controller('car')
 export class CarController {
-    constructor(private CarService: CarService){}
+    constructor(private carService: CarService){}
 
     
     @Get()
     async gerCars(){
-        return.this.carService.getCars();
+        return this.carService.getCars();
     }
 }
